69da05a8f0b5b3eae87ac76bb1a0341457d63c32
Rezki Cyprien <cyprien.rezki@hotmail.fr>
2022-06-14 17:05:26 +0200

    Start new project from lewagon/react-boilerplate

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Hello = ({ name }) => {
+  return (
+    <div>
+      Hello,
+      {name}
+    </div>
+  );
+};
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(<Hello name="World" />, root);
+}
